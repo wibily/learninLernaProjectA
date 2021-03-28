304610fdfd4c1e50c2e4ad91517d1a769f343b1c
William Cheung <william.cheung@data61.csiro.au>
2021-03-28 13:05:59 +1100

    working craco

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Foo } from "@wibily/lernacommon/src/widgets/Foo.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Foo />
+      <div>Check out Project A</div>
+    </div>
+  );
+}
+
+export default App;
